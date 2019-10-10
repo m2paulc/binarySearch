10c6f2bb9ca98ce15d728f4ad69f11a4c38ecd64
M C <hyp3rsp8c3@gmail.com>
2019-10-10 04:01:00 -0400

    fixed npm packages vunerability and modified arguments to be more understandable

--- binarySearch.js
+++ binarySearch.js
@@ -1,13 +1,13 @@
-function binarySearch(arr, num) {
+function binarySearch(arrSorted, val) {
   //define starting and ending point (low and high point)
   let left = 0;
-  let right = arr.length - 1;
+  let right = arrSorted.length - 1;
   //define the middle point to start the search
   let mid = Math.floor((left + right) / 2);
   //loop thru the array until target is found and starting point never reaches to the ending point
-  while (num !== arr[mid] && left <= right) {
+  while (val !== arr[mid] && left <= right) {
     //see if target is less than the middle of the array to determine search (left or right)
-    if (num < arr[mid]) {
+    if (val < arrSorted[mid]) {
       right = mid - 1;
     } else {
       left = mid + 1;
@@ -16,7 +16,7 @@ function binarySearch(arr, num) {
     mid = Math.floor((left + right) / 2);
   }
   //once target is found return the index of the array
-  if (num === arr[mid]) {
+  if (val === arrSorted[mid]) {
     return mid;
   }
   //defaults to -1 if target is not found in the array
